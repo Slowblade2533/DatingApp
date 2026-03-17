ed202058ab14c6bc84cc7a92a9efd2d02f450beb
Slowblade <slowblade@hotmail.com>
2026-03-17 16:57:18 +0700

    First Commit

---
+++ API/Program.cs
@@ -0,0 +1,16 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
